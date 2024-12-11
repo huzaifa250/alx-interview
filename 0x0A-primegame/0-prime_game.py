536a32948a431447b5ea70f5109b20b97ae1a911
huzaifa250 <huz.dark1@gmail.com>
2024-12-11 06:51:12 -0800

    edit game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -25,51 +25,16 @@ def sieve_of_eratosthenes(n):
             print(p)
 
 
-def playGame(n):
-    """
-    Simulate one round of the game for a given number n.
-    Returns the winner of the round: "Maria" or "Ben".
-    """
-    primes = sieve_of_eratosthenes(n)
-    availableNumbers = set(range(2, n + 1))  # Numbers available to pick
-    turn = 0  # 0 for Maria, 1 for Ben (alternate turns)
-
-    while primes:
-        moveMade = False
-        for prime in primes:
-            if prime in availableNumbers:
-                # Remove the prime and its multiples from the available nums
-                for multiple in range(prime, n + 1, prime):
-                    availableNumbers.discard(multiple)
-                moveMade = True
-                break  # Move to the next turn after a move is made
-
-        if not moveMade:
-            break  # If no valid moves are possible, the game ends
-
-        # Switch turns
-        turn = 1 - turn
-
-    # If the game ends, the player who cannot make a move loses
-    return "Maria" if turn == 1 else "Ben"
-
-
 def isWinner(numRounds, roundValues):
-    """
-    Determine the overall winner of the game.
-    """
     if not numRounds or not roundValues:
         return None
-
     mariaScore = benScore = 0
-
     for i in range(numRounds):
-        winner = playGame(roundValues[i])
-        if winner == "Maria":
-            mariaScore += 1
-        elif winner == "Ben":
+        primes = sieve_of_eratosthenes(roundValues[i])
+        if len(primes) % 2 == 0:
             benScore += 1
-
+        else:
+            mariaScore += 1
     if mariaScore > benScore:
         return "Maria"
     elif benScore > mariaScore:
